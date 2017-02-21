0214196d4a9f4b8990effecb885f3d8d5b1ca34a
Fahad Qazi <fahadqazi@gmail.com>
2017-02-21 22:51:27 +0000

    setup requests to openweathermap api

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
     constructor(props){
         super(props)
         this.state={
@@ -18,6 +21,8 @@ export default class SearchBar extends Component{
 
     onFormSubmit(event){
         event.preventDefault();
+        this.props.fetchWeather(event.target.value);
+        this.setState({ term: '' })
     }
 
     render(){
@@ -37,3 +42,9 @@ export default class SearchBar extends Component{
         );
     }
 }
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({ fetchWeather: fetchWeather}, dispatch)
+}
+
+export default connect (null, mapDispatchToProps)(SearchBar)
